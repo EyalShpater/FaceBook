5dfc9f421f0429c7c028e503b6e137905bf1f8bb
Eyal <Eyal@REALITY-PINI>
2022-11-20 09:52:52 +0200

    Add project files.

---
+++ facebook/fansPage.cpp
@@ -0,0 +1,1 @@
+#include "fansPage.h"
---
+++ facebook/main.cpp
@@ -0,0 +1,25 @@
+#pragma warning (disable:4996)
+
+#include <iostream>
+#include <ctime>
+
+#include "time.h"
+#include "date.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "hello" << endl;
+    Time t(22, 00, 32);
+    Date d(19, 11, 2021);
+
+    time_t t1;
+
+    t.show();
+    d.show();
+    t1 = time(nullptr);
+
+    cout << asctime(localtime(&t1));
+    return 0;
+}
