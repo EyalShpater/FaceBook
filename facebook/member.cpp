a862f838b8ffaef0f6b82481285fa2fb80f117f9
Nitzan Sde or <nitzansde@mta.ac.il>
2022-11-21 21:50:39 +0200

    update members cpp

--- facebook/member.cpp
+++ facebook/member.cpp
@@ -17,35 +17,35 @@ Member::~Member()
 
 void Member::addStatusToBillboard(const Status& newStatus)
 {
-
+	theBillboard.push(newStatus);
 }
 
 void Member::addFriend(const Member& newFriend)
 {
-
+	members.push(newFriend);
 }
 
 void Member::cancelFriendship(const char* name)
 {
-
+	members.pop(name);
 }
 
 void Member::likePage(const char* name)
 {
-
+	fansPages.pop(name);
 }
 
 void Member::showAllStatus() const
 {
-
+	theBillboard.showAllStatus();
 }
 
 void Member::showLatest10thStatus() const
 {
-
+	theBillboard.showLatest10thStatus();
 }
 
 void Member::showAllFriend() const
 {
-
+	members.showAllMembers();
 }
