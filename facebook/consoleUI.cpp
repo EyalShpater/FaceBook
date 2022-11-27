d55a281f19b594ee7786132e2dad73c4604032da
Nitzan Sde or <nitzansde@mta.ac.il>
2022-11-27 23:32:41 +0200

    :(

--- facebook/consoleUI.cpp
+++ facebook/consoleUI.cpp
@@ -68,3 +68,81 @@ void ConsoleUI::addDefaultMembersToFacebook()
 	nitzan.showAllFriend();
 	cout << "\n-----------\n";
 }
+
+void ConsoleUI::menu()
+{
+	enum eChoice { ONE = 1, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGTH, NINE, TEN, ELEVEN, TWELVE };
+
+	int choice;
+	cout << "Please enter your choice from the menu:" << endl
+		<< "1- Add member" << endl
+		<< "2- Add fans page" << endl
+		<< "3- Add status to member/fans page" << endl
+		<< "4- Displaying all the statuses of a member / fan page" << endl
+		<< "5- Displaying the 10 most recent statuses of a certain member's friends" << endl
+		<< "6- Friendship link between two friends" << endl
+		<< "7- Cancellation of membership between two members" << endl
+		<< "8- Adding a fan to the page" << endl
+		<< "9- Removing a fan from page" << endl
+		<< "10- Displaying all members registered to faceBook" << endl
+		<< "11- Showing all the friends of a certain member / the fans of a certain page" << endl
+		<< "12-Exit" << endl;
+
+	cin >> choice;
+
+	switch (choice)
+	{
+	case ONE:
+	{
+		addFriend();
+		break;
+	}
+	case TWO:
+	{
+		addFansPage();
+		break;
+	}
+	case THREE:
+	{
+		break;
+	}
+	case FOUR:
+	{
+		break;
+	}
+	case FIVE:
+	{
+		break;
+	}
+	case SIX:
+	{
+		break;
+	}
+	case SEVEN:
+	{
+		break;
+	}
+	case EIGTH:
+	{
+		break;
+	}
+	case NINE:
+	{
+		break;
+	}
+	case TEN:
+	{
+		break;
+	}
+	case ELEVEN:
+	{
+		break;
+	}
+	case TWELVE:
+	{
+		break;
+	}
+
+	}
+
+}
